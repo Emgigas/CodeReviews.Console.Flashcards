c3f867bc4409549614162ae9af75b783cc3fcd3c
Emgigas <119613722+Emgigas@users.noreply.github.com>
2025-08-26 17:29:05 +0100

    Created appsettings.json and DataAccess class to manage access to the Flashcards database

---
+++ Models/DataAccess.cs
@@ -0,0 +1,58 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+
+public class DataAccess
+{
+    // Get connection string for database
+    IConfiguration configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+    private string connectionString;
+
+    public DataAccess()
+    {
+        connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+
+    // Create tables in Database 'Flashcards'
+    internal void CreateTables()
+    {
+        try
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+
+                string createStackTable =
+                    @"IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Stacks')
+                    CREATE TABLE Stacks (
+                        Id int IDENTITY(1,1) NOT NULL,
+                        Name NVARCHAR(30) NOT NULL UNIQUE,
+                        PRIMARY KEY (Id)
+                    );";
+                connection.Execute(createStackTable);
+
+                string createFlashcardTable =
+                    @"IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Flashcards')
+                    CREATE TABLE Flashcards (
+                        Id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+                        Question NVARCHAR(30) NOT NULL,
+                        Answer NVARCHAR(30) NOT NULL,
+                        StackId int NOT NULL
+                            FOREIGN KEY
+                            REFERENCES Stacks(Id)
+                            ON DELETE CASCADE
+                            ON UPDATE CASCADE
+                    );";
+                connection.Execute(createFlashcardTable);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"There was an error: {ex.Message}");
+        }
+    }
+}
